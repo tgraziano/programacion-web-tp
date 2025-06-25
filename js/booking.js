02cbac36f436c95acaf76c0b4f0eb4f83ea2c002
tgraziano <tomas.graziano@davinci.edu.ar>
2025-06-25 00:00:57 -0300

    feat(js): integrar script formulario contacto footer

--- js/booking.js
+++ js/booking.js
@@ -1,3 +1,4 @@
 import "/assets/scripts/countdown.js";
+import "/assets/scripts/form-contact.js";
 import "/assets/scripts/form-reserve.js";
 import "/assets/scripts/menu-responsive.js";
--- js/not-found.js
+++ js/not-found.js
@@ -1,2 +1,3 @@
 import "/assets/scripts/countdown.js";
+import "/assets/scripts/form-contact.js";
 import "/assets/scripts/menu-responsive.js";
